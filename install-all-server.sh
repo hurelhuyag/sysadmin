593f5f3adaf47881f0a131e1956b6be90cc1b39d
hurelhuyag <hurle0409@gmail.com>
2015-08-03 13:21:10 +0800

    Create install-all-server.sh

---
+++ install-all-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get install \
+mariadb-server \
+graphicsmagick \
+openjdk-8-jdk \
+tomcat7 \
+nginx \ 
+fail2ban \
+lxc \
+p7zip-full \
+percona-xtrabackup
