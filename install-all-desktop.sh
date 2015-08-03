e4449b43c01a692793361c188eb6890daaf29f99
hurelhuyag <hurle0409@gmail.com>
2015-08-03 13:20:47 +0800

    Create install-all-desktop.sh

---
+++ install-all-desktop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# essential apps
+sudo apt-get install -y \
+chromium-browser \
+chromium-codecs-ffmpeg-extra \
+ffmpeg \
+filezilla \
+gimp \
+git-core \
+graphicsmagick \
+htop \
+indicator-multiload \
+libappindicator1 \
+mariadb-server \
+maven \
+openjdk-8-jdk \
+openjdk-8-source \
+openjdk-8-doc \
+p7zip-full \
+samba \
+vim \
+vlc \
+lib32z1 \
+lib32stdc++6
+
+# codecs
+sudo apt-get install \ 
+gstreamer1.0-alsa \
+gstreamer1.0-clutter
+gstreamer1.0-libav \
+gstreamer1.0-nice \
+gstreamer1.0-plugins-bad-videoparsers \
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-base-apps \
+gstreamer1.0-plugins-good \
+gstreamer1.0-pulseaudio \
+gstreamer1.0-tools \
+gstreamer1.0-x
