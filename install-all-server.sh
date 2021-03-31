7b3e255f92a14b8f995850da01315711f476cb60
Hurelhuyag <hurelhuyag@gmail.com>
2021-03-31 16:28:15 +0800

    Update install-all-server.sh

--- install-all-server.sh
+++ install-all-server.sh
@@ -9,21 +9,17 @@ sed -i 's/..\.archive\.ubuntu\.com/mirror.datacenter.mn/g' /etc/apt/sources.list
 apt-get update 
 apt-get upgrade
 apt-get install \
-mariadb-server \
-graphicsmagick \
-openjdk-8-jdk \
-tomcat7 \
-nginx \ 
-fail2ban \
-lxc \
-p7zip-full \
-percona-xtrabackup
+  mariadb-server \
+  graphicsmagick \
+  imagemagick \
+  file \
+  openjdk-11-jdk \
+  tomcat9 \
+  nginx \ 
+  fail2ban \
+  p7zip-full
 
 echo 'hide_userland_threads=1' > ~/.htoprc
-
-ln -s /var/lib/tomcat7/common/ /usr/share/tomcat7/common
-ln -s /var/lib/tomcat7/server/ /usr/share/tomcat7/server
-ln -s /var/lib/tomcat7/shared/ /usr/share/tomcat7/shared
 
 adduser sodon
 usermod -a -G tomcat7 sodon
